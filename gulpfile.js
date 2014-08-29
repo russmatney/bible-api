5cf5391d8d20a5735a4e557f0c1d69fdf2d319ba
Russell Matney <russell.matney@gmail.com>
2014-08-29 01:33:42 -0400

    adds gulpfile for live-reload on html/js files

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    shell = require('gulp-shell');
+
+var paths = {
+  views: ['./*/**.html'],
+  scripts: ['./*/**.js']
+};
+
+gulp.task('connect', function() {
+  connect.server({
+    app: [__dirname + "/public"],
+    port: 4000,
+    livereload: true
+  });
+})
+
+gulp.task('html', function() {
+  gulp.src(paths.views)
+    .pipe(connect.reload());
+});
+
+gulp.task('scripts', function() {
+  gulp.src(paths.scripts)
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function() {
+  gulp.watch(paths.views, ['html']);
+  gulp.watch(paths.scripts, ['scripts']);
+});
+
+gulp.task('goserver', shell.task(['gin']));
+
+gulp.task('default', ['connect', 'scripts', 'html', 'watch', 'goserver']);
